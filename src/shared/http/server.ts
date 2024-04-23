349ef08e0ad2eb4ec8b3faf9322eef685fe3af44
Jonathan <jhonyramos46@gmail.com>
2024-04-23 18:32:26 -0300

    feat: adiciona configuração inicial para tratamento de erros

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import routes from "../routes"
+import AppError from "@shared/errors/AppError";
 
 
 const app = express();
@@ -9,6 +10,19 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error: Error, resquest: Request, response: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+  return response.status(500).json({
+    status: "error",
+    message: "internal server error"
+  })
+},)
 
 app.listen(3333, () => {
   console.log("Server is running on port 3333");
